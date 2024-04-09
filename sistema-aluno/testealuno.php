06cd355133879d747660c10d3f25b33d43eaf7f6
mariana-magalhaes <145463957+mariimagalhaesr@users.noreply.github.com>
2024-04-09 11:37:10 -0300

    Add files via upload

---
+++ sistema-aluno/testealuno.php
@@ -0,0 +1,13 @@
+<?php
+var_dump($_GET);
+echo "<br>";
+echo "o nome e". $_GET['nome'];
+echo "<br>";
+echo "o endereco e". $_GET['endereco'];
+echo "<br>";
+echo "o email e". $_GET['email'];
+echo "<br>";
+echo "a senha da conta e" . $_GET['senha'];
+echo "a idade digitada foi" . $_GET['numero'];
+
+
